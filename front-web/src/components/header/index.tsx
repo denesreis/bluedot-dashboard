26043a7987063d65e266592aed5f0598088f564e
Denes Alessandro Reis <denesreis@scasistemas.com.br>
2021-11-19 10:02:25 -0300

    Componente Header Adicionado

---
+++ front-web/src/components/header/index.tsx
@@ -0,0 +1,10 @@
+import './styles.css';
+function Header() {
+	return (
+		<header className="main-header-container">
+			<h1 className="main-header-title">Blue Dot - Dash</h1>
+		</header>
+	);
+}
+
+export default Header;
