d3abaf7c9f4a7ec5050db906a69e29c40ee83ef4
Denes Alessandro Reis <denesreis@scasistemas.com.br>
2021-11-22 12:33:57 -0300

    Componente filter adiocionado

--- front-web/src/components/filter/index.tsx
+++ front-web/src/components/filter/index.tsx
@@ -1,9 +1,35 @@
 import './styles.css';
+import 'flatpickr/dist/themes/material_green.css';
+import FlatPicker from 'react-flatpickr';
+import flatpickrlib from 'flatpickr';
+import { Portuguese } from 'flatpickr/dist/l10n/pt';
+
+flatpickrlib.localize(Portuguese);
 
 function Filter() {
+	const onChangeDate = (dates: Date[]) => {
+		/*O flatpicker retorna uma lista de datas */
+
+		console.log(dates);
+	};
 	return (
 		<div className="filter-container base-card">
-			<h1>Filter</h1>
+			<FlatPicker
+				options={{
+					mode: 'range',
+					dateFormat: 'd/m/y',
+					showMonths: 2
+				}}
+				className="filter-input"
+				onChange={onChangeDate}
+				placeholder="Selecione um período"
+			/>
+			<select className="filter-input">
+				<option value="">Selecione um gênero</option>
+				<option value="MALE">Masculino</option>
+				<option value="FEMALE">Feminino</option>
+				<option value="OTHER">Outro</option>
+			</select>
 		</div>
 	);
 }
