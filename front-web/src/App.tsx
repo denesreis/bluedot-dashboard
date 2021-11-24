040c11769e16cc46ef2721db59034c697a32594b
Denes Alessandro Reis <denesreis@scasistemas.com.br>
2021-11-24 19:23:37 -0300

    Componente pie-chart-card adicionado

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Filter from './components/filter';
 import Header from './components/header';
+import PieChartCard from './components/pie-chart-card';
 import SalesByDate from './components/sales-by-date';
 import SalesSummary from './components/sales-summary';
 
@@ -14,6 +15,16 @@ function App() {
 				<SalesByDate />
 				<div className="sales-overview-container">
 					<SalesSummary />
+					<PieChartCard
+						name="Lojas"
+						labels={['Uberlandia', 'Uberaba', 'Lavras']}
+						series={[40, 30, 60]}
+					/>
+					<PieChartCard
+						name="Pagamento"
+						labels={['Crédito', 'Débito', 'Dinheiro']}
+						series={[60, 30, 20]}
+					/>
 				</div>
 			</div>
 		</>
