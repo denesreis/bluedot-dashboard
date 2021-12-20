15d2870e2c7b138bfbe8426a2b18bb4e631d9c20
Denes Alessandro Reis <denesreis@scasistemas.com.br>
2021-12-20 20:30:11 -0300

    first homolog

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -3,10 +3,10 @@ INSERT INTO tb_category(name) VALUES ('Cosméticos e higiene pessoal');
 INSERT INTO tb_category(name) VALUES ('Utilidades domésticas');
 INSERT INTO tb_category(name) VALUES ('Roupas e acessórios');
 
-INSERT INTO tb_store(name) VALUES('Uberaba');
-INSERT INTO tb_store(name) VALUES('Uberlândia');
-INSERT INTO tb_store(name) VALUES('Araguari');
-INSERT INTO tb_store(name) VALUES('Ituiutaba');
+INSERT INTO tb_store(name) VALUES('Lavras');
+INSERT INTO tb_store(name) VALUES('Divinópolis');
+INSERT INTO tb_store(name) VALUES('Itaúna');
+INSERT INTO tb_store(name) VALUES('Oliveira');
 
 INSERT INTO tb_payment_method(description) VALUES ('Crédito');
 INSERT INTO tb_payment_method(description) VALUES ('Débito');
